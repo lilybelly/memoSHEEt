56a505fe8701997f4e54e04dadc45bbf13f33857
lily_bell514 <lilybell.5555@gmail.com>
2019-02-24 15:49:29 +0900

    Initial Commit

---
+++ memoSHEEt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  memoSHEEt
+//
+//  Created by 石川陽子 on 2019/02/24.
+//  Copyright © 2019年 Yoko Ishikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
